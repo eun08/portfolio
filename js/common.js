ba2b746ff248c4137e136d4c9afe14f5feae2c7d
eun08 <76501271+eun08@users.noreply.github.com>
2021-02-04 16:46:03 +0900

    1646

--- js/common.js
+++ js/common.js
@@ -6,6 +6,21 @@ $(function(){
         } else {
             $('#header').removeClass('tag_box_fixed');
         }
-    })
+    })//scroll
 
-})
+    $('.gnb > li > a').click(function(){
+        $('.gnb > li > a').removeClass('on');
+        $(this).addClass('on');
+
+    })//nav click
+
+    $('.tag_box ul > li > a').click(function(){
+        $('.tag_box ul > li > a').removeClass('on');
+        $(this).addClass('on');
+
+    })//tag click
+
+
+
+
+})//jq
