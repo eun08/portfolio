5c40c1e2e9b5b0716be913e672339d87c0ec628b
eun08 <76501271+eun08@users.noreply.github.com>
2021-02-04 14:57:52 +0900

    1457

--- js/common.js
+++ js/common.js
@@ -1,17 +1,11 @@
-$(document).ready(function(){
-    $('.venobox').venobox(); 
+$(function(){
 
-    $("#firstlink").venobox().trigger('click');
+    $(window).scroll(function(){
+        if($(document).scrollTop() >= 160){
+            $('#header').addClass('tag_box_fixed');
+        } else {
+            $('#header').removeClass('tag_box_fixed');
+        }
+    })
 
-    $('.venobox_custom').venobox({
-        framewidth : '400px',                            // default: ''
-        frameheight: '300px',                            // default: ''
-        border     : '10px',                             // default: '0'
-        bgcolor    : '#5dff5e',                          // default: '#fff'
-        titleattr  : 'data-title',                       // default: 'title'
-        numeratio  : true,                               // default: false
-        infinigall : true,                               // default: false
-        
-    });
-
-  });//jq end
+})
