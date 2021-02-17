e41897bd8b6ad0e6e0628eddfeb5a321aba49132
eun08 <76501271+eun08@users.noreply.github.com>
2021-02-18 04:56:16 +0900

    0456

---
+++ aboutMe.php
@@ -0,0 +1,45 @@
+<!--header 불러오기-->
+<?php include("header.php"); ?>
+
+
+<!--content-->
+	<body>
+		<div id="wrap" class="active">
+			<div id="logo">
+				<h1>
+					<a href="https://eun08.github.io/portfolio/"><img src="images/logo.svg" alt="로고"></a>
+				</h1>
+			</div>
+			<header id="header">
+				  <nav id="nav">
+					<p>PORTFOLIO</p>
+					<ul class="gnb">
+					  <li><a href="https://eun08.github.io/portfolio/" class="works">WORKS</a></li>
+					  <li><a href="https://eun08.github.io/portfolio/aboutMe" class="about on">ABOUT ME</a></li>
+					  <li><a href="https://eun08.github.io/portfolio/aboutMe#div3" class="contact">CONTACT</a></li>
+					</ul>
+				  </nav>  
+				  
+			</header>
+    <section id="about">
+		<div class="container">
+			<div id="div1"><img src="images/illust.jpg" alt="자기소개그림"></div>
+			<div id="div2" class="about_me">
+				<p  class="intro">"안녕하세요, 소통하는 디자이너 허은주입니다."</p>
+				<p class="intro_txt">커뮤니케이션의 중요성을 늘 생각합니다.<br>
+				   충분한 소통과 공감으로 더 나은 디자인을 만들기 위해 노력합니다.</p>
+				<div class="resume"><a href="#" target="_blank">이력서</a></div>   
+			</div>
+			<div id="div3" class="contact">
+				<p class="illust"><img src="images/illust-cup.jpg" alt="cup illust"></p>
+				<p>MOBILE :<span>010 9393 5676</span></p>
+				<p>E-MAIL :<span>blossom0102@naver.com</span></p>
+				<a href="https://eun08.github.io/portfolio/"  target="_blank"><p>WEBSITE :<span>https://eun08.github.io/portfolio/</span></p></a>
+			</div>
+
+		</div>
+	</section>
+    
+
+<!--footer 불러오기-->
+<?php include("footer.php"); ?>
---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer id="footer">
+    <div>
+      <p>© Designer. All Rights Reserved.</p>
+    </div>
+  </footer>
+<div id="mMemu">
+    <div id="mMenu_bg">
+        <div id="mMenu_close">
+            <span></span>
+        </div>
+    </div>
+    <ul>
+        <li><a href="#">WORKS</a></li>
+        <li><a href="#">ABOUT ME</a></li>
+        <li><a href="#">CONTACT</a></li>
+    </ul>
+</div>  
+</div><!--wrap-->	
+
+</body>
+</html>
