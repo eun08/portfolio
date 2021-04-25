d043f8018ed19cfbcd4cf60188eff84cfea99dc6
eun08 <76501271+eun08@users.noreply.github.com>
2021-04-25 21:02:31 +0900

    0425

--- js/common.js
+++ js/common.js
@@ -1,22 +1,22 @@
 $(function(){
 
-    $(window).scroll(function(){
+  $(window).scroll(function(){
         if($(document).scrollTop() >= 160){
             $('#header').addClass('tag_box_fixed');
         } else {
             $('#header').removeClass('tag_box_fixed');
         }
     })//scroll
-
-    $('.gnb > li > a').click(function(){
+  
+   /* $('.gnb > li > a').click(function(){
         $('.gnb > li > a').removeClass('on');
         $(this).addClass('on');
 
-    })//nav click
+    })//nav click*/
 
-    $('.gnb > li > a.about,a.contact').click(function(){        
+    /*$('.gnb > li > a.about,a.contact').click(function(){        
         $('#wrap').addClass('active');
-    })
+    })*/
 
     $('.gnb > li > a.works').click(function(){
         $('#wrap').removeClass('active');
